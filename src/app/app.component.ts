3169042ef07ef0c84ffccce971c2cc5afc47be34
Maria <lancy2118@gmail.com>
2019-10-06 10:10:29 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, Optional } from '@angular/core';
+import { FormControl, FormGroup, Validators,FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-app';
+  loginForm:FormGroup;
+  formSubmitted=false;
+  firstName='';
+  lastName='';
+  address='';
+  address2='';
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+   createForm() {
+    this.loginForm = this.fb.group({
+       firstname: ['', Validators.required ],
+       lastname:['',Validators.required],
+       address: ['', Validators.required ],
+       address2:['',Optional]
+    });
+  }
+  display(){
+    this.formSubmitted=true;
+    this.firstName=this.loginForm.value.firstname;
+    this.lastName=this.loginForm.value.lastname;
+    this.address=this.loginForm.value.address;
+    this.address2=this.loginForm.value.address2;
+  }
+  reset(){
+    this.formSubmitted=false;
+    this.loginForm.reset();
+  }
+  
 }
