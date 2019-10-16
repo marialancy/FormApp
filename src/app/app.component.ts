86f6e98068109aa0e05faa4940ddb80e862def68
Maria <lancy2118@gmail.com>
2019-10-16 11:55:28 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,31 @@
-import { Component, Optional } from '@angular/core';
+import { Component, Optional,OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators,FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  outputs:['inputValueChange']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loginForm:FormGroup;
   formSubmitted=false;
   firstName='';
   lastName='';
   address='';
   address2='';
+  phone='';
+
   constructor(private fb: FormBuilder) {
     this.createForm();
+    
   }
    createForm() {
     this.loginForm = this.fb.group({
-       firstname: ['', Validators.required ],
+       firstname: ['', Validators.required],
        lastname:['',Validators.required],
        address: ['', Validators.required ],
-       address2:['',Optional]
+       address2:['',Optional],
+       phone:['',Optional]
     });
   }
   display(){
@@ -29,10 +34,35 @@ export class AppComponent {
     this.lastName=this.loginForm.value.lastname;
     this.address=this.loginForm.value.address;
     this.address2=this.loginForm.value.address2;
+    this.phone=this.loginForm.value.phone;
+
   }
   reset(){
     this.formSubmitted=false;
     this.loginForm.reset();
   }
+  validate(e:any){
+    //  var phoneNumber=e.target.value.replace(/[^\d]/g,'')
+    //  console.log(phoneNumber)
+    //  this.phone=phoneNumber
+    if (e.target.value.match('[0-9]')){
+      this.phone=e.target.value;
+      if(e.target.value.length==10){
+        this.phoneFormat(this.phone)
+      }
+    }
+    else{
+      this.phone=null;
+      this.loginForm.controls['phone'].reset();
+    }
+  }
+  phoneFormat(phoneValue){
+    phoneValue='('+this.phone.slice(0,3)+') '+this.phone.slice(3,6)+'-'+this.phone.slice(6);
+    this.phone=phoneValue;
+
+  }
+ngOnInit(){
+  
+}
   
 }
